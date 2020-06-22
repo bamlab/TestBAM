12715d5aef5f9a06aec4aa8e9362aa60e5bf91bf
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-06-22 14:07:41 +0200

    Initialize repo

---
+++ TestBAM/TestBAM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBAM
+//
+//  Created by Mathilde Ressier on 22/06/2020.
+//  Copyright © 2020 Mathilde Ressier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
